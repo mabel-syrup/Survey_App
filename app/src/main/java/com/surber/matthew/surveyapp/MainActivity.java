7e61f892bbb888cb05beb4f9acb81dd14be6c39a
mjSurber <matthewsurber96@gmail.com>
2016-09-06 13:57:13 -0500

    Added a comment.

---
+++ app/src/main/java/com/surber/matthew/surveyapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.surber.matthew.surveyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //A comment
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
